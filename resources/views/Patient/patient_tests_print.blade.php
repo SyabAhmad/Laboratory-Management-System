3aff6ff2472a02f71a2100453fa00811e1aa35ed
SyabAhmad <syabblogger@gmail.com>
2025-11-24 15:06:12 +0500

    feat: Enhance print layout for multiple test reports with improved spacing and styling adjustments

--- resources/views/Patient/patient_tests_print.blade.php
+++ resources/views/Patient/patient_tests_print.blade.php
@@ -1,10 +1,19 @@
 @extends('Layout.print')
 @section('title', 'Multiple Test Reports - Print')
 @section('content')
-    <div class="print-body">
-        @foreach($testEntries as $testEntry)
-            @include('Patient.partials.test_report', ['patient' => $patient, 'testEntry' => $testEntry])
-            <div style="height:12mm; width:100%;"></div>
+    <div class="print-body {{ count($testEntries) > 1 ? 'multiple-tests' : '' }}">
+        {{-- Patient info once at top --}}
+        @if(!empty($testEntries))
+            @php $firstTest = $testEntries[0]; @endphp
+            @include('Patient.partials.test_report', ['patient' => $patient, 'testEntry' => $firstTest, 'skipPatientInfo' => false])
+        @endif
+
+        {{-- Then each additional test data without patient info --}}
+        @foreach($testEntries as $index => $testEntry)
+            @if($index > 0)
+                <div style="height:6mm; width:100%;"></div> <!-- reduced separation -->
+                @include('Patient.partials.test_report', ['patient' => $patient, 'testEntry' => $testEntry, 'skipPatientInfo' => true])
+            @endif
         @endforeach
     </div>
     <script>
